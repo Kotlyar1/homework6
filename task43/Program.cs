1a42a097ea8b4d3f7bb0d8f2f0b176f67d49787c
Александр Котляров <bony17@bk.ru>
2022-08-11 19:22:23 +0300

    решение задачи 43

---
+++ task43/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.Write("Введите число для k1: ");
+string k1 = Console.ReadLine();
+Console.Write("Введите число для k2: ");
+string k2 = Console.ReadLine();
+Console.Write("Введите число для b1: ");
+string b1 = Console.ReadLine();
+Console.Write("Введите число для b2: ");
+string b2 = Console.ReadLine();
+
+void ExceptionHandling(string a1, string a2, string c1, string c2)
+{
+    bool yesInt = double.TryParse(a1, out double d1);
+    bool yesInt1 = double.TryParse(a2, out double d2);
+    bool yesInt2 = double.TryParse(c1, out double f1);
+    bool yesInt3 = double.TryParse(c2, out double f2);
+
+    if (yesInt && yesInt1 && yesInt2 && yesInt3)
+    {
+        double x = (f2 - f1) / (d1 - d2);
+        double y = ((f2 * d1) - (f1 * d2)) / (d1 - d2);
+        Console.BackgroundColor = ConsoleColor.DarkRed;
+        Console.WriteLine($" x = {x:F1}; y = {y:F1} ");
+        Console.BackgroundColor = ConsoleColor.Black;
+        
+    }
+    else
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.Write($"{a1}, {a2}, {c1}, {c2}");
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.WriteLine(" -> Некорректный ввод!");
+    }
+}
+
+ExceptionHandling(k1, k2, b1, b2);
